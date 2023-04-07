9a0083bc8c2d88a31363b85ec80f2ae9fdf329df
rm-rfvi <129233522+rm-rfvi@users.noreply.github.com>
2023-04-07 12:49:11 +0930

    Update provisionarr.sh

--- provisionarr.sh
+++ provisionarr.sh
@@ -20,9 +20,10 @@ calibre_web_server="calibre.axfr.cc"
 sabnzbd_server="sabnzbd.axfr.cc"
 whisparr_server="whisparr.axfr.cc"
 autobrr_server="autobrr.axfr.cc"
+qbittorrent_server="qbit.axfr.cc"
 
 # Define the lines to add to /etc/fstab
-line1="10.0.0.10:/mnt/Pool0/scratch     /mnt/scratch   nfs    defaults,user,rw           0       0"
+line1="10.0.0.10:/mnt/Pool0/scratch     /mnt/download   nfs    defaults,user,rw           0       0"
 line2="10.0.0.10:/mnt/Pool0/media0        /mnt/media      nfs    defaults,user,rw        0       0"
 
 # Create directories
@@ -39,8 +40,6 @@ sudo mkdir -p /opt/docker/run/whisparr
 sudo mkdir -p /opt/docker/run/readarr
 sudo mkdir -p /opt/docker/run/calibre_svc
 sudo mkdir -p /opt/docker/run/calibre
-
-
 sudo mkdir -p /opt/docker/run/flaresolverr
 sudo mkdir -p /opt/docker/run/sonarr
 sudo mkdir -p /opt/docker/run/jackett
@@ -60,9 +59,11 @@ sudo mkdir -p /opt/docker/run/sabnzbd
 
 sudo mkdir -p /opt/docker/run/rutorrent
 
-sudo mkdir -p /opt/nginx
+sudo mkdir -p /opt/docker/run/nginx
 sudo mkdir -p /mnt/download
 sudo mkdir -p /mnt/media
+sudo mkdir -p /mnt/scratch
+
 # Check if the lines are already present in /etc/fstab
 if grep -Fxq "$line1" /etc/fstab && grep -Fxq "$line2" /etc/fstab; then
     echo "Lines already exist in /etc/fstab"
@@ -72,238 +73,3 @@ else
     echo "Lines added to /etc/fstab"
 fi
 
-# Create nginx jackett config file
-sudo tee /opt/nginx/jackett.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $jackett_server;
-
-  location / {
-    proxy_pass  http://jackett:9117;
-  }
-}
-
-EOF
-
-# Create nginx flaresolverr config file
-sudo tee /opt/nginx/flaresolverr.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $flaresolverr_server;
-
-  location / {
-    proxy_pass  http://flaresolverr:8191;
-  }
-}
-
-EOF
-
-# Create nginx sonarr config file
-sudo tee /opt/nginx/sonarr.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $sonarr_server;
-
-  location / {
-    proxy_pass  http://sonarr:8989;
-  }
-}
-
-EOF
-
-
-# Create nginx radarr config file
-sudo tee /opt/nginx/radarr.conf > /dev/null <<EOF
-
-
-server {
-  listen        80;
-  server_name   $radarr_server;
-
-  location / {
-    proxy_pass  http://radarr:7878;
-  }
-}
-
-EOF
-
-# Create nginx prowlarr2 config file
-sudo tee /opt/nginx/prowlarr.conf > /dev/null <<EOF
-
-
-server {
-  listen        80;
-  server_name   $prowlarr_server;
-
-  location / {
-    proxy_pass  http://prowlarr:9696;
-  }
-}
-
-EOF
-
-# Create nginx lidarr config file
-sudo tee /opt/nginx/lidarr.conf > /dev/null <<EOF
-
-
-server {
-  listen        80;
-  server_name   $lidarr_server;
-
-  location / {
-    proxy_pass  http://lidarr:8686;
-  }
-}
-
-EOF
-
-# Create nginx ombi config file
-sudo tee /opt/nginx/ombi.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $ombi_server;
-
-  location / {
-    proxy_pass  http://ombi:3579;
-  }
-}
-
-EOF
-
-# Create nginx portainarr config file
-sudo tee /opt/nginx/portainarr.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $portainarr_server;
-
-  location / {
-    proxy_pass  http://portainarr:9000;
-  }
-}
-
-EOF
-
-
-# Create nginx requestrr config file
-sudo tee /opt/nginx/requestrr.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $requestrr_server;
-
-  location / {
-    proxy_pass  http://192.168.6.2:4545;
-  }
-}
-
-EOF
-# Create nginx bazarr config file
-sudo tee /opt/nginx/bazarr.conf > /dev/null <<EOF
-
-
-server {
-  listen        80;
-  server_name   $bazarr_server;
-
-  location / {
-    proxy_pass  http://192.168.14.2:6767;
-  }
-}
-
-EOF
-
-# Create nginx overseerr config file
-sudo tee /opt/nginx/overseer.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $overseerr_server;
-
-  location / {
-    proxy_pass  http://192.168.15.2:5055;
-  }
-}
-
-EOF
-
-
-
-# Create nginx notifiarr config file
-sudo tee /opt/nginx/notifiarr.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $notifiarr_server;
-
-  location / {
-    proxy_pass  http://192.168.16.2:5454;
-  }
-}
-
-EOF
-# Create nginx nzbhydra config file
-sudo tee /opt/nginx/nzbhydra.conf > /dev/null <<EOF
-
-server {
-  listen        80;
-  server_name   $nzbhydra_server;
-
-  location / {
-    proxy_pass  http://192.168.17.2:5076;
-  }
-}
-
-EOF
-
-
-
-# Create nginx readarr config file
-sudo tee /opt/nginx/readarr.conf > /dev/null <<EOF
-
-
-server {
-  listen        80;
-  server_name   $readarr_server;
-
-  location / {
-    proxy_pass  http://192.168.18.2:8787;
-  }
-}
-
-EOF
-
-
-# Create nginx sabnzbd config file
-sudo tee /opt/nginx/sabnzbd.conf > /dev/null <<EOF
-
-
-server {
-  listen        80;
-  server_name   $sabnzbd_server;
-
-  location / {
-    proxy_pass  http://readarr:8080;
-  }
-}
-
-EOF
-
-# Create nginx sabnzbd config file
-sudo tee /opt/nginx/whisparr.conf > /dev/null <<EOF
-
-
-server {
-  listen        80;
-  server_name   $whisparr_server;
-
-  location /whisparr/ {
-    proxy_pass  http://whisparr:6969;
-  }
-}
-
-EOF
